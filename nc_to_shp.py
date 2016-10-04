76202c2b9c9991b159400c5058adb6582b0015ba
Tommy <trob745@aucklanduni.ac.nz>
2016-10-05 12:59:41 +1300

    Updated main python file. Now handles all files ending with .nc in a directory. I've been thinking about the most efficient way to read and write from the .nc and .csv files. I've tried numpy's loadtxt function however tests show this to be slower for me.

--- nc_to_shp.py
+++ nc_to_shp.py
@@ -1,30 +1,55 @@
+import os
 import numpy as np
 import csv
+import time
 from netCDF4 import Dataset
 
-rootgrp = Dataset("C:/Github/WS10_VCSN_xaijz_2006-2010c0.nc", "r", format="NETCDF4")
-var = rootgrp["ws10"]
-lat = rootgrp["latitude"][:]
-long = rootgrp["longitude"][:]
-time = rootgrp["time"][:]
+firstRead = 1
+relativepath = "X:\\Hamilton\\SpatialLabBaseData\\NZ\\Climate\\CCII"
 
-csvfile = open("C:/Github/table.csv", "rb")
-reader = csv.reader(csvfile)
-outputfile = open("c:/Github/output.csv", "wb")
-writer = csv.writer(outputfile)
-row = reader.next()
-row.append("var")
-writer.writerow(row)
-for row in reader:
-	# latitude index
-	print row
-	lati = np.argmin(np.abs(lat - np.float32(row[2])))
-	# longitude index
-	longi = np.argmin(np.abs(long - np.float32(row[3])))
-	print "H"
-	row.append(var[1][lati][longi])
-	writer.writerow(row)
+t0 = time.clock()
+for subdir, dirs, files in os.walk(relativepath):
+  for file in files:
+    filepath = os.path.join(subdir, file)
+		
+    if filepath.endswith(".nc"):
+      if firstRead:
+        fileEnding = "table.csv"
+        firstRead = 0
+      else:
+        fileEnding = "output.csv"
+      print (filepath) +"\n", os.path.realpath("") + os.sep + fileEnding + "\n", os.path.realpath("") + os.sep + "output.csv"
+      rootgrp = Dataset(filepath, "r", format="NETCDF4")
+      lats = rootgrp["latitude"][:]
+      longs = rootgrp["longitude"][:]
+      timeData = rootgrp["time"][:]
+      varTitle = ""
+      datapoint = rootgrp["time"][:]
 
-csvfile.close()
-outputfile.close()
+      for var in iter(rootgrp.variables):
+			
+        if var != "longitude" and var != "latitude" and var != "elevation" and var != "time_bnds":
+					datapoint = rootgrp[var]
+					varTitle = var
+      
+      with open(os.path.realpath("") + os.sep + fileEnding, "rb") as inputfile:
+        with open(os.path.realpath("") + os.sep + "temp.csv", "wb") as outputfile:
+          reader = csv.reader(inputfile)
+          writer = csv.writer(outputfile)
+          row = reader.next()
+          row.append(varTitle)
+          writer.writerow(row)
+        
+          for row in reader:
+       
+            # latitude index
+            lati = np.argmin(np.abs(lats - np.float32(row[2])))
+            # longitude index
+            longi = np.argmin(np.abs(longs - np.float32(row[3])))
+            row.append(datapoint[1][lati][longi])
+            writer.writerow(row)
 
+      os.remove(os.path.realpath("") + os.sep + "output.csv")
+      os.rename(os.path.realpath("") + os.sep + "temp.csv", os.path.realpath("") + os.sep + "output.csv")
+      rootgrp.close()
+print time.clock()-t0
