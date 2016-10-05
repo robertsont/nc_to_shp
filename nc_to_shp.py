f63284296b1288160708b4ca72a0de6ca16d8e0f
Tommy <trob745@aucklanduni.ac.nz>
2016-10-06 11:58:32 +1300

    Updated to handle netCDF4 variables correctly (previously, elements were being access incorrectly via variable_array[time][lat][long] as opposed to variable_array[time,lat,long]. Values are obtained on whether the lat and long values are close enough rather than finding the index which gives the smallest difference. Values are now averaged over the time dimension rather than taking the first element. May run faster on a linux machine as I'm unsure whether the netCDF4 wheel obtained from PyPI installs the package with cython support (I don't think it does but am unsure and I am not able to build the netCDF4 package from source on my windows machine).

--- nc_to_shp.py
+++ nc_to_shp.py
@@ -4,52 +4,96 @@ import csv
 import time
 from netCDF4 import Dataset
 
-firstRead = 1
-relativepath = "X:\\Hamilton\\SpatialLabBaseData\\NZ\\Climate\\CCII"
+#Error class for use when two values are have coordinates that are
+#too close to distinguish as different points
+class LatLongError(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
 
-t0 = time.clock()
+#Boolean for determinig whether to read from table.csv or output.csv
+firstRead = True
+relativepath = "X:\\Hamilton\\SpatialLabBaseData\\NZ\\Climate\\CCII\\RCP2.6"
+#relativepath = "C:\\Github\\nc_to_shp\\data\\RCP2.6"
+
+#Traverse through all files in all subdirectories in the path specified above
 for subdir, dirs, files in os.walk(relativepath):
-  for file in files:
-    filepath = os.path.join(subdir, file)
-		
+  for filename in files
+    #Capture file name for use as variable name prefix in output.csv
+    varprefix = filename[:-3]
+    filepath = os.path.join(subdir, filename)
+
+    #Only open netCDF4 files
     if filepath.endswith(".nc"):
       if firstRead:
         fileEnding = "table.csv"
-        firstRead = 0
+        firstRead = False
       else:
         fileEnding = "output.csv"
-      print (filepath) +"\n", os.path.realpath("") + os.sep + fileEnding + "\n", os.path.realpath("") + os.sep + "output.csv"
-      rootgrp = Dataset(filepath, "r", format="NETCDF4")
+      #print (filepath) +"\n", os.path.realpath("") + os.sep + fileEnding + "\n", os.path.realpath("") + os.sep + "output.csv"
+
+      #Variable for determining time taken for each file to be read
+      t0 = time.clock()
+      #Open netCDF4 file and store variables
+      rootgrp = Dataset(filepath, "r", format="NETCDF4", diskless=True)
       lats = rootgrp["latitude"][:]
       longs = rootgrp["longitude"][:]
       timeData = rootgrp["time"][:]
+      #Vraible title to follw varprefix in output.csv
       varTitle = ""
+      #Initialise array to contian the variable of interest from the netCDF4 file
       datapoint = rootgrp["time"][:]
 
-      for var in iter(rootgrp.variables):
-			
-        if var != "longitude" and var != "latitude" and var != "elevation" and var != "time_bnds":
-					datapoint = rootgrp[var]
-					varTitle = var
-      
+      #Since the variable name contained in the netCDF4 is not known apriori, find it by excluding all known variables
+      for var in iter(rootgrp.variables):		
+        if var != "longitude" and var != "latitude" and var != "elevation" and var != "time_bnds" and var != "time":
+	  datapoint = rootgrp[var]
+	  varTitle = var
+        
+      print "File time: ", time.clock() - t0
+      #Variable for determining time taken for full computation to take place
+      t0 = time.clock()
+      #Open csvs to read from and write to
       with open(os.path.realpath("") + os.sep + fileEnding, "rb") as inputfile:
         with open(os.path.realpath("") + os.sep + "temp.csv", "wb") as outputfile:
-          reader = csv.reader(inputfile)
           writer = csv.writer(outputfile)
-          row = reader.next()
-          row.append(varTitle)
+          #Extract elements from csv text to an array
+          row = inputfile.next().split(",")
+          #Final element is terminated with a carriage return character and new line character, so remove these two characters (\r\n)
+          row[len(row)-1] = row[len(row)-1][:-2]
+          #Place variable name as first element in new column
+          row.append(varprefix + "_" + varTitle)
           writer.writerow(row)
-        
-          for row in reader:
-       
-            # latitude index
-            lati = np.argmin(np.abs(lats - np.float32(row[2])))
-            # longitude index
-            longi = np.argmin(np.abs(longs - np.float32(row[3])))
-            row.append(datapoint[1][lati][longi])
-            writer.writerow(row)
+          try:
+            for row in inputfile:
+              #Extract elements from csv text to an array
+              row = row.split(",")
+              #Final element is terminated with a carriage return character and new line character, so remove these two characters (\r\n)
+              row[len(row)-1] = row[len(row)-1][:-2]
+              # latitude index
+              lati = np.argwhere(np.abs(lats - np.float32(row[2]))< 0.01)
+              # longitude index
+              longi = np.argwhere(np.abs(longs - np.float32(row[3]))< 0.01)
+              #If no value with coordinates close enough is found, write a missing element to csv
+              if len(lati) == 0 or len(longi) == 0: 
+                row.append(np.ma.masked)
+              #If more than one value with coordinates close enough is found, print terminated in csv and skip to next file
+              elif len(lati) > 1 or len(longi) > 1:
+                row.append("Terminated")
+                writer.writerow(row)
+                raise LatLongError("Existence of two values with either latitude or longitude values within 0.01 degrees")
+              else:
+                row.append(datapoint[:,lati[0],longi[0]].mean())
+              writer.writerow(row)
+          except LatLongError as e:
+              print e.value, "Skipping rest of file. Occured at ", filepath
 
+      #Replace old output.csv with new csv (named temp but renamed to output)    
       os.remove(os.path.realpath("") + os.sep + "output.csv")
       os.rename(os.path.realpath("") + os.sep + "temp.csv", os.path.realpath("") + os.sep + "output.csv")
+      #Release the netCDF4 file object
       rootgrp.close()
-print time.clock()-t0
+      print "Computation time: ", time.clock() - t0    
+    
+
