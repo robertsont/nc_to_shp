b7a11c524a8423beb7880d834e7a3170e080514a
Tommy <trob745@aucklanduni.ac.nz>
2016-10-19 17:16:40 +1300

    Added the .pyc instead of .py for previous commit.

--- nc_to_shpv7.py
+++ nc_to_shpv7.py
@@ -1,20 +1,7 @@
-import os
-import sys
+import sys, os, time, csv, traceback
 import numpy as np
-import csv
-import time
 from netCDF4 import Dataset, num2date
 import scipy.io.netcdf as scinet
-#import psutil
-import math
-
-#Error class for use when two values are have coordinates that are
-#too close to distinguish as different points
-class LatLongError(Exception):
-  def __init__(self, value):
-    self.value = value
-  def __str__(self):
-    return repr(self.value)
 
 # Print iterations progress
 def printProgress (iteration, total, prefix = '', suffix = '', timetaken = 0, timerecent=0, filetime=0):
@@ -34,60 +21,53 @@ def getSize(start_path):
     return total_size
 
 def writeTopRow(row, timeslice, varTitle, years, filename):
-  #Final element is terminated with a carriage return character and new line character, so remove these two characters (\r\n)
-  row[len(row)-1] = row[len(row)-1][:-2]
+  #Final element is terminated with a new line character, so remove it
+  row[len(row)-1] = row[len(row)-1][:-1]
   #Place variable name as first element in new column
   row.append(filename[:-3] + "_" + varTitle)
-  numberOfYears =  len(set(years))/(timeslice)+(len(set(years))%timeslice != 0)
+  #Based on timeslice, figure out the division of years for the file
+  numberOfYears =  int(len(set(years))/(timeslice)+(len(set(years))%timeslice != 0))
   if len(set(years)) > timeslice:
-    row = row + [`(years[0]+(timeslice)*(x-1))` + "-" + `(years[0]+(timeslice)*x-1)` for x in range(2,numberOfYears)] + [`(years[0]+(timeslice)*(numberOfYears-1))` + "-" + `years[years.size-1]`] # for x in range(0, len(set(years))%(timeslice) != 0)]
+    row = row + [str(years[0]+(timeslice)*(x-1)) + "-" + str(years[0]+(timeslice)*x-1) for x in list(range(2,numberOfYears))] + [str(years[0]+(timeslice)*(numberOfYears-1)) + "-" + str(years[years.size-1])] # for x in range(0, len(set(years))%(timeslice) != 0)]
   return row, numberOfYears
 
 def fileReadWrite(datapoint, lats, longs, fileloc, tempfile, timeslice, years, varTitle, filename):
-  with open(fileloc, "rb") as inputfile:
-    with open(tempfile, "wb") as outputfile:
+  with open(fileloc, "r") as inputfile:
+    with open(tempfile, "w", newline='') as outputfile:
       writer = csv.writer(outputfile)
-      row, numberOfYears = writeTopRow(inputfile.next().split(","), timeslice, varTitle, years, filename)
+      row, numberOfYears = writeTopRow(next(inputfile).split(","), timeslice, varTitle, years, filename)
       writer.writerow(row)
-      length = len(lats)
-      try:
-        inputfile.next() 
-        for row in inputfile:
-          row = row.split(",")
-          row[len(row)-1] = row[len(row)-1][:-2]
-          lati = np.argwhere(np.abs(lats - np.float32(row[2]))< 0.01)
-          longi = np.argwhere(np.abs(longs - np.float32(row[3]))< 0.01)
-          if len(lati) == 0 or len(longi) == 0:
-            row = row + [np.ma.masked for k in range(0, numberOfYears, 1)]
-            writer.writerow(row)
-          else:
-            lati = lati[0,0]
-            longi = longi[0,0]
-            timej = 0
-            for j in range(0,numberOfYears):
-              timejo = timej
-              year = years[timejo]
-              try:
-                timej = np.argwhere(years == years[timejo]+timeslice)[0][0]
-              except IndexError:
-                timej=years.size
-              row.append(datapoint[timejo:timej,lati,longi].mean())
-            writer.writerow(row)
-      except LatLongError as e:
-        print e.value, "Skipping rest of file. Occured at ", filepath
+      for row in inputfile:
+        row = row.split(",")
+        row[len(row)-1] = row[len(row)-1][:-1]
+        lati = np.argwhere(np.abs(lats - np.float32(row[2]))< 0.01)
+        longi = np.argwhere(np.abs(longs - np.float32(row[3]))< 0.01)
+        if len(lati) == 0 or len(longi) == 0:
+          row = row + [np.ma.masked for k in range(0, numberOfYears)]
+          writer.writerow(row)
+        else:
+          timej = 0
+          for j in range(0,numberOfYears):
+            timejo = timej
+            year = years[timejo]
+            try:
+              timej = np.argwhere(years == years[timejo]+timeslice)[0][0]
+            except IndexError:
+              timej=years.size
+            row.append(datapoint[timejo:timej,lati[0][0],longi[0][0]].mean())
+          writer.writerow(row)
 
 def extractData(filepath):
-  t3 = time.clock()
   rootgrp = scinet.netcdf_file(filepath, "r", False)
-  return rootgrp, rootgrp.variables["latitude"][:], rootgrp.variables["longitude"][:], np.asarray([getattr(x, "year") for x in [num2date(y,rootgrp.variables["time"].units) for y in rootgrp.variables["time"][:]]]), time.clock()-t3
+  return rootgrp, rootgrp.variables["latitude"][:].copy(), rootgrp.variables["longitude"][:].copy(), np.asarray([getattr(x, "year") for x in [num2date(y,rootgrp.variables["time"].units.decode('utf8')) for y in rootgrp.variables["time"][:]]])
 
 def runDataAvg(inputfileloc, outputfileloc, tempfile, timeslice, relativepath):
-  #Boolean for determinig whether to read from table.csv or output.csv
+  #Boolean for determining whether to read from table.csv or output.csv
   firstRead = True
   totalsize = getSize(relativepath)
   log = []
 
-  print "Directory size", totalsize/(1024*1024*1024), "GB"
+  print("Directory size", totalsize/(1024*1024*1024), "GB")
   currentsize = 0
   printProgress(currentsize, totalsize, "Completed so far:",)
 
@@ -106,22 +86,24 @@ def runDataAvg(inputfileloc, outputfileloc, tempfile, timeslice, relativepath):
         else:
           fileloc = outputfileloc
         try:
-          #tt = time.clock()
-          rootgrp, lats, longs, years, t3 = extractData(filepath)
-          #print time.clock()-tt
-          for var in iter(rootgrp.variables):		
-            if var != "longitude" and var != "latitude" and var != "elevation" and var != "time_bnds" and var != "time":
-              fileReadWrite(rootgrp.variables[var][:,:,:], lats, longs, fileloc, tempfile, timeslice, years, var, filename)
-              rootgrp.close()
+          t3 = time.clock()
+          rootgrp, lats, longs, years = extractData(filepath)
+          t3 = time.clock()-t3
+          for var in iter(rootgrp.variables):
+            if var != 'longitude' and var != 'latitude' and var != 'elevation' and var != 'time_bnds' and var != 'time' and var != 'x_index' and var != 'y_index' and var != 'time_bounds':
+              try:
+                fileReadWrite(rootgrp.variables[var][:,:,:].copy(), lats, longs, fileloc, tempfile, timeslice, years, var, filename)
+              except (KeyError, IndexError) as e:
+                log = log + [e, var, filename]
+          rootgrp.close()
            
           os.remove(outputfileloc)
           os.rename(tempfile, outputfileloc)
-          rootgrp.close()
           printProgress(currentsize, totalsize, "Completed so far:", "Time taken: ", time.clock() - t0, time.clock() - t1, t3)
 
         except OverflowError as e:
           log = log + [e, filename]
-
-  print log
+          
+  print(log)
     
 
