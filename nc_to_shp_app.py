560e052fff569f922a44caebe5398816ebd0e6d3
Tommy <trob745@aucklanduni.ac.nz>
2016-12-15 09:07:14 +1300

    Minor changes in _app file.

--- nc_to_shp_app.py
+++ nc_to_shp_app.py
@@ -2,7 +2,7 @@ import nc_to_shpv9 as nc
 
 if __name__ == "__main__":
   timeslice = int(input("Please enter a time range: "))
-  nc.runDataAvg("C:\\Github\\NetCDF4 file reader\\nc_to_shp\\table.csv", "C:\\Github\\NetCDF4 file reader\\nc_to_shp\\output.csv", timeslice, "X:\\Hamilton\\SpatialLabBaseData\\NZ\\Climate\\CCII")
+  nc.runDataAvg("C:\\Github\\NetCDF4 file reader\\nc_to_shp\\table.csv", "C:\\Github\\NetCDF4 file reader\\nc_to_shp\\output.csv", timeslice, "X:\\Hamilton\\SpatialLabBaseData\\NZ\\Climate\\CCII\\RCP2.6")
   input("Finished")
 
 
